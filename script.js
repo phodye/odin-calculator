11e4e04a993c0fbbaab56cfbde226ec2a1abd4ad
Andrew Dye <phodye@gmail.com>
2022-05-12 16:16:31 -0400

    Created initial math functions

---
+++ script.js
@@ -0,0 +1,25 @@
+//initial calculator functions
+
+//adding 
+const addition = function(a, b) {
+    let c = a + b;
+    return c;
+}
+
+//subtracting 
+const subtraction = function(a, b) {
+    let c = a - b;
+    return c;
+}
+
+//multiplication
+const multiply = function(a, b) {
+    let c = a * b;
+    return c;
+}
+
+//division
+const divide = function(a, b) {
+    let c = a / b;
+    return c;
+}
